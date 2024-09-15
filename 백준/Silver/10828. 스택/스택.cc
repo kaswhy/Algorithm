f5fa1aa94e5bf177bec8860c53c69afa91a0772e
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2024-09-15 22:43:52 +0900

    [Silver IV] Title: 스택, Time: 204 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,50 +1,54 @@
-#include <iostream>
-#include <stack>
+/*
+ * 입력: 명령 수 n / 명령
+ * 출력: 출력해야 할 시, 한 줄에 하나씩 출력
+ * STL stack으로 구현
+ */
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	stack<int> st;
+    int n;
+    cin >> n;
 
-	while (n--) {
-		string s;
-		cin >> s;
+    stack<int> s;
+    while(n--){
+        string com;
+        cin >> com;
 
-		if (s == "push") {
-			int x;
-			cin >> x;
-			st.push(x);
-		}
-
-		else if (s == "pop") {
-			if (st.empty()) {
-				cout << -1 << "\n";
-				continue;
-			}
-			cout << st.top() << "\n";
-			st.pop();
-		}
-
-		else if (s == "size") {
-			cout << st.size() << "\n";
-		}
-
-		else if (s == "empty") {
-			if (st.empty()) {
-				cout << 1 << "\n";
-				continue;
-			}
-			cout << 0 << "\n";
-		}
-
-
-		else if (s == "top") {
-			if (st.empty()) {
-				cout << -1 << "\n";
-				continue;
-			}
-			cout << st.top() << "\n";
-		}
-	}
+        if(com == "push"){
+            int x;
+            cin >> x;
+            s.push(x);
+            continue;
+        }
+        if(com == "pop"){
+            if(s.empty()){
+                cout << -1 << "\n";
+                continue;
+            }
+            cout << s.top() << "\n";
+            s.pop();
+            continue;
+        }
+        if(com == "size"){
+            cout << s.size() << "\n";
+            continue;
+        }
+        if(com == "empty"){
+            if(s.empty()){
+                cout << 1 << "\n";
+                continue;
+            }
+            cout << 0 << "\n";
+            continue;
+        }
+        if(com == "top"){
+            if(s.empty()){
+                cout << -1 << "\n";
+                continue;
+            }
+            cout << s.top() << "\n";
+            continue;
+        }
+    }
 }
