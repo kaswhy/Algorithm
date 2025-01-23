6b35b0d96825e22c66548e5917ed17611d007df1
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-01-23 16:11:07 +0900

    [Silver V] Title: 숫자 카드, Time: 208 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -5,26 +5,24 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+
     int n;
     cin >> n;
 
-    set<int> s;
+    vector<int> v;
 
     while(n--){
         int x;
         cin >> x;
-        s.insert(x);
+        v.push_back(x);
     }
-    
+    sort(v.begin(), v.end());
 
     int m;
     cin >> m;
     while(m--){
         int x;
         cin >> x;
-
-        if(s.find(x) == s.end()) cout << 0 << " ";
-        else cout << 1 << " ";
+        cout << binary_search(v.begin(), v.end(), x) << " ";
     }
 }
