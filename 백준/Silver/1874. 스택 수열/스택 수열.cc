2c3185f74bac5d3bbe1e516efe7e5d92838bba80
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-03-02 22:08:01 +0900

    [Silver II] Title: 스택 수열, Time: 16 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,69 +1,50 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    /*
-     * [push, push, push, push, pop, pop, push, push, pop, push, push, pop, pop, pop, pop, pop]
-     *      1 / 2 / 5 / 7 / 8/
-     *      4 / 3 / 6 / 8 / 7/ 5 / 2/ 1
-     *
-     *      1 / 2 / 5 / 3/ 4
-     *
-     *  입력 :  n 개의 숫자
-     *  출력 : 명령어 순서 or NO
-     *  1 ~ n까지의 배열 만들어놓기 -> 십만
-     *  큐에 명령어 넣어보기
-     */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     int n;
     cin >> n;
-
-    queue<int> nums; // 1 ~ n
-    vector<int> inputs; // 특정 수열
-    vector<int> making; // 해당 수열 만드는 과정 -> push
-    vector<int> result; // 결과 -> pop
-    queue<char> commands; // 명령어
-
-    int chk = 1;
-
-    for(int i = 0; i<n; i++){
-        nums.push(i+1);
-
-        int k;
-        cin >> k;
-        inputs.push_back(k);
+    vector<int> a(n, 0);
+    vector<char> results;
+    
+    for(int i =0 ; i<n; i++) {
+        cin >> a[i];
     }
-
-    for(int i = 0; i<n; i++){
-        int now = inputs[i]; // 3
-        int comp = making.empty()? 0: making[making.size()-1]; //
-        if(comp < now) {
-            while (!nums.empty() && nums.front() <= now) {
-                making.push_back(nums.front());
-                commands.push('+');
-                nums.pop();
+    
+    stack<int> s;
+    int num = 1;
+    bool result = true;
+    
+    for(int i = 0; i<a.size(); i++) {
+        int now = a[i];
+        
+        if(now >= num) {
+            while(now >= num) {
+                s.push(num++);
+                results.push_back('+');
             }
-            result.push_back(making[making.size()-1]);
-            making.pop_back();
-            commands.push('-');
+            s.pop();
+            results.push_back('-');
+            continue;
         }
-        else if (comp == now) {
-            result.push_back(making[making.size()-1]);
-            making.pop_back();
-            commands.push('-');
-        }
-        else {
-            chk = 0;
+        int c = s.top();
+        s.pop();
+        
+        if(c > now) {
+            cout << "NO";
+            result = false;
             break;
         }
+        results.push_back('-');
     }
-
-    if(chk) {
-        for (int i = 0; i < n*2; i++) {
-            cout << commands.front() << "\n";
-            commands.pop();
+    
+    if(result) {
+        for(int i = 0; i<results.size(); i++) {
+            cout << results[i] << '\n';
         }
     }
-    else cout << "NO";
 }
